ebb4fdbae6dd1313b9dc0cb703bb9464c1d7ab77
Sara Ghaedi <saraghaedi75@gmail.com>
2021-11-23 14:58:14 +0100

    switched to react

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import cx from "classnames";
+
+export default function App() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div>
+      <div
+        className={cx("searchbar", {
+          "is-open": open,
+        })}
+      >
+        <input
+          placeholder="Search"
+          onFocus={(e) => setOpen(true)}
+          onBlur={(e) => setOpen(false)}
+        />
+        <div className="dropdown_positioner">
+          <div className="dropdown">
+            <ul>
+              <li>
+                <strong>re</strong>al life CSS transition
+              </li>
+              <li>
+                <strong>Re</strong>act hooks
+              </li>
+              <li>
+                pa<strong>re</strong>ntheses
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <p>Use the searchbar above</p>
+    </div>
+  );
+}
